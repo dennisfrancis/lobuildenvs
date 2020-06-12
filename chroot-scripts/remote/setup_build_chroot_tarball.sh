98ceb5b5d56fbeeefd01fb3883d36a56196ee3c3
Dennis Francis <dennisfrancis.in@gmail.com>
2020-06-12 19:53:56 +0530

    buildmc -> chrootbuildmc

--- chroot-scripts/remote/setup_build_chroot_tarball.sh
+++ chroot-scripts/remote/setup_build_chroot_tarball.sh
@@ -2,33 +2,33 @@
 
 source ../envvars.sh
 
-echo "updating buildmc"
-ssh root@buildmc "apt-get update && apt-get full-upgrade -y && shutdown -r now"
+echo "updating chrootbuildmc"
+ssh root@chrootbuildmc "apt-get update && apt-get full-upgrade -y && shutdown -r now"
 
-[ $? -ne 0 ] && { echo "buildmc update/reboot failed! Not proceeding."; exit -1; }
+[ $? -ne 0 ] && { echo "chrootbuildmc update/reboot failed! Not proceeding."; exit -1; }
 
-echo "Waiting 10s for buildmc to reboot..."
+echo "Waiting 10s for chrootbuildmc to reboot..."
 sleep 10s
 
 
-echo "setting up buildmc for building a chroot"
-ssh root@buildmc "apt-get install rsync git && cd /root && git clone ${LOBUILDENVS_REPO_URL}"
+echo "setting up chrootbuildmc for building a chroot"
+ssh root@chrootbuildmc "apt-get install rsync git && cd /root && git clone ${LOBUILDENVS_REPO_URL}"
 
-[ $? -ne 0 ] && { echo "setting up buildmc failed! Not proceeding."; exit -1; }
+[ $? -ne 0 ] && { echo "setting up chrootbuildmc failed! Not proceeding."; exit -1; }
 
 
 SETUPDIR="/root/${LOBUILDENVS_REPO_NAME}/chroot-scripts"
 
-echo "building chroot and LO dev-env in buildmc"
-ssh root@buildmc "cd \"${SETUPDIR}\" && bash build-chroot-background.sh"
+echo "building chroot and LO dev-env in chrootbuildmc"
+ssh root@chrootbuildmc "cd \"${SETUPDIR}\" && bash build-chroot-background.sh"
 
-[ $? -ne 0 ] && { echo "chroot/LOdev-env build in buildmc failed! Not proceeding."; exit -1; }
+[ $? -ne 0 ] && { echo "chroot/LOdev-env build in chrootbuildmc failed! Not proceeding."; exit -1; }
 
 
-echo "creating chroot tarball in buildmc"
-ssh root@buildmc "cd \"${SETUPDIR}\" && bash create-chroot-tarball.sh"
+echo "creating chroot tarball in chrootbuildmc"
+ssh root@chrootbuildmc "cd \"${SETUPDIR}\" && bash create-chroot-tarball.sh"
 
-[ $? -ne 0 ] && { echo "chroot tarball generation in buildmc failed! Not proceeding."; exit -1; }
+[ $? -ne 0 ] && { echo "chroot tarball generation in chrootbuildmc failed! Not proceeding."; exit -1; }
 
 
 echo "Chroot tarball is ready in the remote machine."
