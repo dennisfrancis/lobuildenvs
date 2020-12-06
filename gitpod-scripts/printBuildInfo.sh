acf2dca2052534b9f2cda7c195bce01435c1b0d8
Dennis Francis <dennisfrancis.in@gmail.com>
2020-12-06 22:23:25 +0530

    gitpod scripts/makefile

--- gitpod-scripts/printBuildInfo.sh
+++ gitpod-scripts/printBuildInfo.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-REPODIR=${REPODIR-core}
+source ./envvars.sh
 
-WORKSPACE="/workspace"
-COREROOT="${WORKSPACE}/${REPODIR}"
-MAKEROOTPID=$(ps aux | grep build-nocheck | grep -v grep | awk '{print $2}')
-
-echo ""
+MAKEROOTPID=$(pgrep -a make | head -1 | cut -f 1 -d' ')
 
 if [ -z $MAKEROOTPID ]
 then
-	echo "Build status : No build in progress"
+	echo "Build status : No builds in progress"
 else
-	TZ=Asia/Kolkata ps -p ${MAKEROOTPID} -o pid,cmd,etime,lstart
+	echo "Build status : Builds in progress"
 fi
 
 echo ""
 echo "core.git build space usage"
-du -sh ${COREROOT}/workdir ${COREROOT}/instdir
+du -sh ${COREDIR}/workdir ${COREDIR}/instdir
+echo "online.git build space usage"
+du -sh ${ONLINEDIR}/
 
 echo ""
-echo "free -h"
 free -h
 
 echo ""
 echo "clang processes"
 pgrep -l clang
 
+true
