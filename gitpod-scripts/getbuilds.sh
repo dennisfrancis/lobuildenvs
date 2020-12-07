38c571f5a683b4e9056ec0b3c9cdc15b35838cdb
Dennis Francis <dennisfrancis.in@gmail.com>
2020-12-07 13:42:22 +0530

    fix getbuilds.sh

--- gitpod-scripts/getbuilds.sh
+++ gitpod-scripts/getbuilds.sh
@@ -2,7 +2,7 @@
 
 source ./envvars.sh
 
-[ $# -eq 1 ] || { echo "Usage $0 <address>" && exit -1; }
+[ $# -eq 2 ] || { echo "Usage $0 <address> <port>" && exit -1; }
 
 echo "Gzipping log files..."
 cd ${WORKSPACE} && gzip -f *.log
@@ -14,19 +14,19 @@ echo "---List of files in ${WORKSPACE}---------"
 cd ${WORKSPACE} && TZ=Asia/Kolkata ls -lhta
 
 echo "Copying gzip files to $1"
-scp ${WORKSPACE}/*.gz "$1":${WORKSPACE}/
+scp -P $2 ${WORKSPACE}/*.gz "$1":${WORKSPACE}/
 
 echo "Copying checksum file"
-scp ${WORKSPACE}/${CSUMSFILE} "$1":${WORKSPACE}/
+scp -P $2 ${WORKSPACE}/${CSUMSFILE} "$1":${WORKSPACE}/
 
 echo "Checksum test"
-ssh "$1" "cd ${WORKSPACE} && sha256sum -c ${CSUMSFILE}"
+ssh -p $2 "$1" "cd ${WORKSPACE} && sha256sum -c ${CSUMSFILE}"
 
 echo "Extracting tarballs"
-ssh "$1" "cd ${WORKSPACE} && tar -xf ${CORE_BUILD_TARBALL} && tar -xf ${ONLINE_BUILD_TARBALL}"
+ssh -p $2 "$1" "cd ${WORKSPACE} && tar -xf ${CORE_BUILD_TARBALL} && tar -xf ${ONLINE_BUILD_TARBALL}"
 
 echo "Syncing file timestamps (both core and online)"
-cd ${WORKSPACE} && rsync -vrt --size-only --existing --exclude '.git' ${COREDIR}/ "$1":${COREDIR}/
-cd ${WORKSPACE} && rsync -vrt --size-only --existing --exclude '.git' ${ONLINEDIR}/ "$1":${ONLINEDIR}/
+cd ${WORKSPACE} && rsync -vrt --size-only --existing --exclude '.git' --port $2 ${COREDIR}/ "$1":${COREDIR}/
+cd ${WORKSPACE} && rsync -vrt --size-only --existing --exclude '.git' --port $2 ${ONLINEDIR}/ "$1":${ONLINEDIR}/
 
 echo "Done!"
